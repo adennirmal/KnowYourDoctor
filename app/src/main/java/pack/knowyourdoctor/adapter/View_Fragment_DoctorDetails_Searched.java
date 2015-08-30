91f26f44251cda567b60f91dd496681df231b2c1
yasi <pyasini6@gmail.com>
2015-08-30 14:55:43 +0530

    yasini test

--- app/src/main/java/pack/knowyourdoctor/adapter/View_Fragment_DoctorDetails_Searched.java
+++ app/src/main/java/pack/knowyourdoctor/adapter/View_Fragment_DoctorDetails_Searched.java
@@ -51,7 +51,7 @@ public class View_Fragment_DoctorDetails_Searched extends Fragment {
     ArrayList<Model_Doctor> searchedDoctors;
     View rootView;
     String passedURL, searched_reg_no;
-    String test;
+    String test123;
     /*
     public View_Fragment_DoctorDetails_Searched(String passedURL) {
         this.passedURL = passedURL;
